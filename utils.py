4839aecf8f82e7da100555203b0300ad8d60d523
srseen <seereen.2540@gmail.com>
2025-06-01 11:27:52 +0700

    Expand image file type support in is_image_file function

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 def is_image_file(filename):
     ext = filename.lower().split('.')[-1]
-    return ext in ['jpg', 'jpeg', 'png', 'heic']
+    return ext in ['jpg', 'jpeg', 'png', 'heic', 'gif','GIF','mov', 'mp4', 'avi', 'mkv', 'webp', 'bmp', 'tiff', 'tif']
 
 def get_extension(filename):
     return filename.lower().split('.')[-1]
